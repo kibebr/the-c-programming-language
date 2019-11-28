c0970de42c3e4c9cd3a670d7e7fcfe399a554316
kibe <47016873+kibebr@users.noreply.github.com>
2019-11-28 18:58:04 -0500

    Updated exercise1-05.c

--- chapter1/exercise1-05.c
+++ chapter1/exercise1-05.c
@@ -18,4 +18,10 @@ int main(void){
 
   for(fahr = 300; fahr >= 0; fahr -= 20)
     printf("%3d %6.1f\n", fahr, ( (5.0/9.0) * (fahr-32) ) );
+  
 }
+
+// fahr is only being used inside the for scope, so it could be rewritten as such:
+
+for(int fahr = 300; fahr >= 0; fahr -= 20)
+  printf("%3d %6.1f\n", fahr, ( (5.0/9.0) * (fahr-32) ) );
