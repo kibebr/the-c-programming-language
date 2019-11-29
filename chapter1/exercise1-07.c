0597e8a94ff6076f6ea8c41056dd20334f0cda08
kibe <47016873+kibebr@users.noreply.github.com>
2019-11-28 23:34:59 -0500

    Create exercise1-07.c

---
+++ chapter1/exercise1-07.c
@@ -0,0 +1,15 @@
+/*
+
+  EXERCISE DESCRIPTION:
+  Exercise 01-7: Write a program to print the value of EOF.
+
+*/
+
+
+#include <stdio.h>
+
+int main(void){
+  printf("%i\n", EOF);
+}
+
+// OUTPUT: -1
