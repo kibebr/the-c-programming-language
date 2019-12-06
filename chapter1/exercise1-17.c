fd149d566a15500f195416001f383451f175af9a
kibe <47016873+kibebr@users.noreply.github.com>
2019-12-05 20:11:17 -0500

    Update exercise1-17.c

--- chapter1/exercise1-17.c
+++ chapter1/exercise1-17.c
@@ -1,8 +1,9 @@
 /*
 
   Exercise 1-17: Write a program to print all input lines that are longer than 80 characters.
-
+  
 */
+
 #include <stdio.h>
 
 #define MAX_ARRAYCHARACTERS 1000
@@ -10,26 +11,24 @@
 
 int main(void){
   
-  int currchar,
-      currcharlen = 0;
+	int currchar,
+		currcharlen = 0;
 
-  char currline[MAX_ARRAYCHARACTERS];
+	char currline[MAX_ARRAYCHARACTERS];
 
-  while( ( currchar = getchar() ) != EOF ){
-    currline[currcharlen] = currchar;
-    ++currcharlen;
+	while( ( currchar = getchar() ) != EOF ){
+		currline[currcharlen] = currchar;
+		++currcharlen;
     
-    if(currchar == '\n'){
-      if(currcharlen >= MIN_CHARACTERS){
-	puts("LINE WITH 80+ CHARACTERS: ");
-	for(int thischar = 0; thischar < currcharlen; ++thischar)
-	  printf("%c", currline[thischar]);
-      }
-      currcharlen = 0;
-    }
-  }
-
-  return 0;
+		if(currchar == '\n'){
+			if(currcharlen >= MIN_CHARACTERS){
+				puts("LINE WITH 80+ CHARACTERS: ");
+				for(int thischar = 0; thischar < currcharlen; ++thischar)
+					printf("%c", currline[thischar]);
+			}
+			currcharlen = 0;
+		}
+	}
   
+	return 0;
 }
-  
