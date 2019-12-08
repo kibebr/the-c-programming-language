a060e8cbec86f676559c4a93af75bcbb3a99d51e
kibe <47016873+kibebr@users.noreply.github.com>
2019-12-07 23:36:02 -0500

    Update exercise1-17.c

--- chapter1/exercise1-17.c
+++ chapter1/exercise1-17.c
@@ -1,34 +1,46 @@
 /*
 
   Exercise 1-17: Write a program to print all input lines that are longer than 80 characters.
-  
+
 */
 
 #include <stdio.h>
 
-#define MAX_ARRAYCHARACTERS 1000
 #define MIN_CHARACTERS 80
 
 int main(void){
-  
-	int currchar,
-		currcharlen = 0;
+	
+	int currchar, // current character being read
+		currlinelen = 0; // length of the line being read
 
-	char currline[MAX_ARRAYCHARACTERS];
+	int characters[MIN_CHARACTERS];
 
-	while( ( currchar = getchar() ) != EOF ){
-		currline[currcharlen] = currchar;
-		++currcharlen;
-    
-		if(currchar == '\n'){
-			if(currcharlen >= MIN_CHARACTERS){
-				puts("LINE WITH 80+ CHARACTERS: ");
-				for(int thischar = 0; thischar < currcharlen; ++thischar)
-					printf("%c", currline[thischar]);
+	while( ( currchar = getchar() ) != EOF){
+
+		if(currlinelen >= MIN_CHARACTERS){ // if the minimum length is achieved, print everything until next line ('\n')
+
+			for(int thischar = 0; thischar < MIN_CHARACTERS; ++thischar) // prints everything stored in buffer
+				putchar(characters[thischar]);
+			
+			
+			do{
+				putchar(currchar);
+			}while( ( currchar = getchar() ) != '\n'); // prints all next characters until a new line
+			
+			currlinelen = 0;
+			
+		}
+		else{
+			
+			if(currchar == '\n'){ // if there's a new line and we the min length hasn't been achieved, reset everything
+				currlinelen = 0;
+				continue;
 			}
-			currcharlen = 0;
+			
+			characters[currlinelen] = currchar; // stores current character in the array
+			++currlinelen;
+			
 		}
 	}
-  
 	return 0;
 }
