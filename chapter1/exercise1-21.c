1f495ff5a3d40e5d17b1adac0c0cf7f033b1c814
kibe <0763722@students.dadeschools.net>
2020-04-03 20:28:31 -0400

    Finished 'exercise1-21'

--- chapter1/exercise1-21.c
+++ chapter1/exercise1-21.c
@@ -1,22 +1,47 @@
-// TODO
+/*	
+	Exercise 1-21: Write a program entab that replaces strings of blanks by the minimum number of tabs and blanks
+	to achieve the same spacing. Use the same tab stops as for detab. When either a tab or a single blank would
+	suffice to reach the stop, which should be given preference?
+*/
+
+// . -> blanks
+// - -> tabs
 
 #include <stdio.h>
-
-#define TAB 4
+#define TAB 8
 
 int main(void){
-	int currchar = 0;
-	int charcount, blankcount = 0;
 
-	while( ( currchar = getchar() ) != EOF){
-		
-		if(currchar == '\n'){
-			charcount = 0;
-			putchar('\n');
+	int currchar, nspaces, nchars;
+	currchar = nspaces = nchars = 0;
+
+	while( (currchar = getchar()) != EOF){
+
+		if(currchar == '.'){
+			++nspaces;
 		}
+
+		while(nspaces >= TAB - nchars){
+			putchar('\t');
+			nspaces -= TAB;
+		}
+
+		while(nspaces > 0){
+			putchar(' ');
+			--nspaces;
+		}
+
+		nchars = 0;
+
+		if(currchar != '\t'){
+			++nchars;
+		}
+
+		putchar(currchar);
 	}
-	
+
 	return 0;
+
 }
 
 /*
